04600bddece3c05c58d253b501feb2df6b727b65
vedku <57610056+vedku@users.noreply.github.com>
2023-03-01 08:18:23 +0800

    Update ContentView.swift

--- initial commits/v3/ContentView.swift
+++ initial commits/v3/ContentView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
- 
+
 struct ContentView: View {
     @State var progress: CGFloat = 0
     @State var name: String = ""
     @State var numberOfDays: Int = 365
     @State var isDarkMode = false
- 
+    
     var body: some View {
         VStack {
             VStack {
@@ -17,7 +17,7 @@ struct ContentView: View {
                             .shadow(color: Color.gray.opacity(0.8), radius: 5, x: 0, y: 2)
                     )
                     .foregroundColor(.black)
- 
+                
                 TextField("Enter the number of days", value: $numberOfDays, formatter: NumberFormatter())
                     .keyboardType(.numberPad)
                     .padding()
@@ -28,7 +28,7 @@ struct ContentView: View {
                     )
                     .foregroundColor(.black)
             }
- 
+            
             Circle()
                 .trim(from: 0, to: progress)
                 .stroke(Color.green, lineWidth: 20)
@@ -40,23 +40,23 @@ struct ContentView: View {
                             .bold()
                             .padding(.bottom, 10)
                             .foregroundColor(isDarkMode ? Color.white : Color.black)
- 
+                        
                         Text("\(Int(progress * CGFloat(numberOfDays))) day\(Int(progress * CGFloat(numberOfDays)) == 1 ? "" : "s")")
                             .font(.title)
                             .bold()
                             .foregroundColor(isDarkMode ? Color.white : Color.black)
- 
+                        
                         Text("\(String(format: "%.2f", (progress * 100)))% done")
                             .font(.caption)
                             .foregroundColor(isDarkMode ? Color.white : Color.black)
- 
+                        
                         Text("of \(numberOfDays) days")
                             .font(.caption)
                             .foregroundColor(isDarkMode ? Color.white : Color.black)
                     }
                 )
                 .padding()
- 
+            
             HStack(spacing: 50) {
                 Button(action: {
                     if progress > 0 {
@@ -70,7 +70,7 @@ struct ContentView: View {
                         .background(Color.red)
                         .cornerRadius(10)
                 }
- 
+                
                 Button(action: {
                     self.progress += 1/CGFloat(self.numberOfDays)
                 }) {
@@ -83,15 +83,15 @@ struct ContentView: View {
                 }
             }
             .padding(.top, 20)
- 
+            
             Spacer()
- 
+            
             Button(action: {
                 self.isDarkMode.toggle()
             }) {
                 Image(systemName: isDarkMode ? "sun.max.fill" : "moon.fill")
                     .font(.title)
-                    .foregroundColor(.primary)
+                    .foregroundColor(isDarkMode ? .white : .primary)
             }
         }
         .padding()
@@ -99,7 +99,7 @@ struct ContentView: View {
         .foregroundColor(isDarkMode ? Color.white : Color.black)
     }
 }
- 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
